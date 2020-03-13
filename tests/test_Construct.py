36dbf228797fd345a23c0942d3f0fc35bf2037ff
Max von Hippel <maxvonhippel1996@gmail.com>
2020-03-13 13:34:56 -0700

    All tests are now passing.:

--- tests/test_Construct.py
+++ tests/test_Construct.py
@@ -3,10 +3,10 @@ import Construct as c
 
 class TestConstruct(unittest.TestCase):
 
-    s_Q   = 'demo/Cole/smallDemo1/Q.pml'
-    s_P   = 'demo/Cole/smallDemo1/P.pml'
-    s_IO  = 'demo/Cole/smallDemo1/IO.txt'
-    s_Phi = 'demo/Cole/smallDemo1/Phi.pml'
+    s_Q   = 'demo/livenessExample1/Q.pml'
+    s_P   = 'demo/livenessExample1/P.pml'
+    s_IO  = 'demo/livenessExample1/IO.txt'
+    s_Phi = 'demo/livenessExample1/Phi.pml'
     IO    = sorted(list(c.getIO(s_IO)))
 
     s_network, s_label = c.makeDaisy(IO,s_Q)
@@ -21,7 +21,13 @@ class TestConstruct(unittest.TestCase):
             'active proctype daisy () {\n\tdo\n\t:: channel!C;\n\t:: channel?A;\n\t:: channel?B;\n\tod\n}')
 
     def test_makeDaisyPhiFinite(self):
-        d_phi = c.makeDaisyPhiFinite(self.s_label, self.s_Phi)
+        d_phi = " ".join([b for b in                              \
+                    [a.strip() for a in                           \
+                    c.makeDaisyPhiFinite(self.s_label, self.s_Phi)\
+                     .replace("\n", "")                           \
+                     .replace("\t", " ")                          \
+                     .split(" ")] if len(b) > 0])
+
         self.assertEqual(\
             d_phi, \
-            "ltl newPhi {\n\talways ( ( b == 1 ) implies\n\t\t\n\teventually always ( w == 0 )\n )\n}")
+            "ltl newPhi { always ( ( b == 1 ) implies ( eventually ( always ( w == 0 ) ) ) )}")
