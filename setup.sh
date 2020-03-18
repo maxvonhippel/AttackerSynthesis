927b87eb3c0c8bec342571efff0e256c6bb55574
Max von Hippel <maxvonhippel1996@gmail.com>
2020-03-17 19:15:06 -0700

    Added commands to make missing dirs

--- setup.sh
+++ setup.sh
@@ -8,7 +8,16 @@
 # ==============================================================================
 
 # Install Spin
-git clone https://github.com/nimble-code/Spin.git && cd Spin && make install 
+git clone https://github.com/nimble-code/Spin.git && cd Spin && make install && cd ..
+# If this doesn't work, the binary is written to Spin/Src/spin, so you can put it in
+# $HOME/.local/bin/. and then everything should work property, if you are on a *nix
+# system.
 
 # Installs green so you can run the tests with my make targets
 pip3 install green
+
+# Makes a dir to write output to
+mkdir -p out
+
+# Makes a dir to write logs to
+mkdir -p logs
